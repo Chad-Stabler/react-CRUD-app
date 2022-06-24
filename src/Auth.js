92cff453181ee074ef8a67f291b51234c57b4b6d
Chad Stabler <stablerpsn@gmail.com>
2022-06-24 18:42:17 -0500

    sign up form complete

--- src/Auth.js
+++ src/Auth.js
@@ -1,3 +1,32 @@
+import { useState } from 'react';
+import { signIn, signUp } from './services/fetch-utils';
+
 export default function Auth({ setUser }) {
-  return (<></>);
+  const [signUpPassword, setSignUpPassword] = useState('');
+  const [signUpEmail, setSignUpEmail] = useState('');
+  const [signInPassword, setSignInPassword] = useState('');
+  const [signInEmail, setSignInEmail] = useState('');
+
+  async function signUpSubmit(e) {
+    e.preventDefault();
+
+    const user = await signUp(signUpEmail, signUpPassword);
+
+    setUser(user);
+  }
+  return (<>
+    <h2>Your movie CRUD app</h2>
+    <form onSubmit={signUpSubmit}>
+      <p>Sign Up</p>
+      <label>
+            Email:
+        <input value={signUpEmail} onChange={e => setSignUpEmail(e.target.value)} type='email' />
+      </label>
+      <label>
+        Password:
+        <input value={signUpPassword} onChange={e => setSignUpPassword(e.target.value)} type='password' />
+      </label>
+      <button>Sign Up</button>
+    </form>
+  </>);
 }
--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -5,3 +5,19 @@ export async function getMovies() {
 
   return checkError(response);
 }
+
+export async function signUp(email, password) {
+  const { user, error } = await client.auth.signUp({ email: email, password: password });
+  if (error) {
+    console.error(error);
+    throw error;
+  } else return user;
+}
+
+export async function signIn(email, password) {
+  const { user, error } = await client.auth.signUp({ email: email, password: password });
+  if (error) {
+    console.error(error);
+    throw error;
+  } else return user;
+}
